f9061f6b57741da5b2f7e1f5f21e56abafc2b051
Mary <sukham@myumantioba.ca>
2020-11-02 13:12:52 -0600

    Fix integration

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 let i = 0;
-function increment() {
+function increment(){
   return ++i;
 }
 
+window.onload = function(){
+  let modal = document.getElementById("myModal");
+  let btn = document.getElementById("myBtn");
+  let close = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  close.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
